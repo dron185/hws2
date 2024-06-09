ad0d7d9b2ca7946a900e7de491ce6dadbf56e9e0
Andrei Huryn <gurin.nord@gmail.com>
2024-06-09 21:17:16 +0300

    complete task 8

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -4,17 +4,19 @@ type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
     switch (action.type) {
         case 'sort': { // by name
-
-            return state // need to fix
+            const direction = action.payload === 'up' ? 1 : -1
+            return [...state].sort((a, b) => direction * a.name.localeCompare(b.name))
+            //return state // need to fix
         }
         case 'check': {
 
-            return state // need to fix
+            return state.filter(u => u.age >= action.payload) // need to fix
         }
         default:
             return state
     }
 }
+
